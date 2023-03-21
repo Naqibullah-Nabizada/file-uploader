1f1ee571cc3c92db7d8362e13a9fabc21385d19b
Naqibullah <naqibullah1400@gmail.com>
2023-03-21 12:03:28 +0430

    create method done in front

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("uploader", "root", "", {
+const db = new Sequelize("image_uploader", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
